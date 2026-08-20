27f67b063281f6a78906adec44a8ae1a83af06cd
Ahmed Khan <khanbial13@gmail.com>
2026-08-20 17:30:49 -0400

    Form structure and its css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import './App.css'
 
 function App() {
-
   return (
-    <>
-      Form 2
-    </>
+    <main className="page">
+      <div className="card">
+        <header>
+          <h1 id="registration-heading" className="card__heading">
+            Registration Details
+          </h1>
+        </header>
+
+        <section className="card__body" >
+          <p className="card__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+
+          <p className="card__text">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+
+          <p className="card__text">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta
+            sunt explicabo.
+          </p>
+        </section>
+      </div>
+    </main>
   )
 }
 
