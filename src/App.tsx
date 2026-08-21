92ba4c5ce7c9721aeefe9a685ac02d3bc01e2daa
Ahmed Khan <khanbial13@gmail.com>
2026-08-20 20:28:01 -0400

    Layout for the frist section of the form

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,31 @@ function App() {
           </h1>
         </header>
 
-        <section className="card__body" >
-          <p className="card__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
 
-          <p className="card__text">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-            non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.
-          </p>
+        <div className="form-layout">
+          <div className="form-row">
+            <div className="form-field placeholder">full width</div>
+          </div>
 
-          <p className="card__text">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae
-            ab illo inventore veritatis et quasi architecto beatae vitae dicta
-            sunt explicabo.
-          </p>
-        </section>
+          <div className="form-row">
+            <div className="form-field placeholder">half</div>
+            <div className="form-field placeholder">half</div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-field form-field--half placeholder">
+              half, rest empty
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-field form-field--half placeholder">
+              half, rest empty
+            </div>
+          </div>
+        </div>
+
+        <hr className="card__divider" />
       </div>
     </main>
   )
